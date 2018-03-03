0416ec0f662a05627b15c2957359aabfacf7ef56
zazapeta <sami.ghazouane@gmail.com>
2018-03-03 11:52:29 +0100

    Added 'store.dispatch' test Added transform object rest spread plugin Added enzyme

--- src/RxStore.test.js
+++ src/RxStore.test.js
@@ -1,8 +1,13 @@
-import RxStore from './RxStore';
 import React from 'react';
+import { shallow } from 'enzyme';
+
+import setup from './setup_test';
+import RxStore from './RxStore';
 import { getComponentName } from './utils';
 
-describe('RxStore', () => {
+beforeAll(setup);
+
+describe('RxStore constructor connect', () => {
   test('should initialize a new store', () => {
     let store = new RxStore();
     expect(store instanceof RxStore).toBe(true);
@@ -50,5 +55,39 @@ describe('RxStore', () => {
     expect(connectedDumb.displayName).toBe(
       `[${ns}]rxConnected(${getComponentName(Dumb)})`,
     );
+  });
+});
+
+describe('RxStore dispatch', () => {
+  test('should change the state of connected comp after a dispatch', () => {
+    let ns = 'Users';
+    let initialState = {
+      users: [],
+    };
+    let store = new RxStore({ ns, initialState });
+    let Dumb = (props) => <div />;
+    let mapStateToProps = (state) => ({
+      users: state.users,
+      title: 'Props added in the fly',
+    });
+    let ConnectedDumb = store.connect(mapStateToProps)(Dumb);
+    let wrapper = shallow(<ConnectedDumb />);
+
+    expect(wrapper.props()).toEqual({
+      users: [],
+      title: 'Props added in the fly',
+    });
+
+    expect(wrapper.state()).toEqual(store.state);
+
+    store.dispatch((state) => ({
+      ...state,
+      users: state.users.concat('Skywalker'),
+    }));
+
+    expect(wrapper.state()).toEqual({
+      ...store.state,
+      users: ['Skywalker'],
+    });
   });
 });
